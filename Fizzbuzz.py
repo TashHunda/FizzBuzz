228e94606b8ef2e2131f832d07f2d6cb94ee4be0
Tash Hunda <Tash Hunda>
2022-07-01 18:11:45 -0500

    final clean version

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,23 +1,17 @@
 #loop for 100 times i.e. range 
-for fizzbuzz in range(100):  
+for fizzbuzz in range(1,101):  
   
-    # divisible by 3 & 5, print 'FizzBuzz'  
-    # instead of number 
-    if fizzbuzz % 15 == 0:  
-        print("FizzBuzz")                                          
-        continue
-  
-    # divisible by 3, print 'Fizz' 
-    # instead of number 
-    elif fizzbuzz % 3 == 0:      
+  # divisible by 3, print 'Fizz' instead of number 
+    if fizzbuzz % 3 == 0:      
         print("Fizz")                                          
-        continue
-  
-    # divisible by  5, print 'Buzz' 
-    # instead of number 
+
+    # divisible by  5, print 'Buzz' instead of number 
     elif fizzbuzz % 5 == 0:          
         print("Buzz")                                      
-        continue
-  
-    # print numbers 
+
+     # divisible by 3 & 5, print 'FizzBuzz' instead of number 
+    elif fizzbuzz % 15 == 0:  
+        print("FizzBuzz")                                          
+    
+    # print numbers! 
     print(fizzbuzz) 
