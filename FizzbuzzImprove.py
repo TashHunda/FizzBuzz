29113cdd2243ec035c536219da639bd3347a3749
Tash Hunda <tashhunda@Tashs-MacBook-Air.local>
2022-07-01 13:43:22 -0500

    Add FizzbuzzImprove.py file

---
+++ FizzbuzzImprove.py
@@ -0,0 +1,21 @@
+#loop for 100 times i.e. range 
+for fizzbuzz in range(1,101):  
+  
+  # divisible by 3, print 'Fizz' instead of number 
+    if fizzbuzz % 3 == 0:      
+        print("Fizz")                                          
+        continue
+
+    # divisible by  5, print 'Buzz' instead of number 
+    elif fizzbuzz % 5 == 0:          
+        print("Buzz")                                      
+        continue
+  
+     # divisible by 3 & 5, print 'FizzBuzz' instead of number 
+    elif fizzbuzz % 15 == 0:  
+        print("FizzBuzz")                                          
+        continue
+  
+    
+    # print numbers 
+    print(fizzbuzz) 
