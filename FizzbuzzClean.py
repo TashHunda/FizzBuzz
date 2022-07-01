3b0721ecfec8676c50240a446e0a6441da060d59
Tash Hunda <tashhunda@Tashs-MacBook-Air.local>
2022-07-01 14:47:39 -0500

    final version

---
+++ FizzbuzzClean.py
@@ -0,0 +1,17 @@
+#loop for 100 times i.e. range 
+for fizzbuzz in range(1,101):  
+  
+  # divisible by 3, print 'Fizz' instead of number 
+    if fizzbuzz % 3 == 0:      
+        print("Fizz")                                          
+
+    # divisible by  5, print 'Buzz' instead of number 
+    elif fizzbuzz % 5 == 0:          
+        print("Buzz")                                      
+
+     # divisible by 3 & 5, print 'FizzBuzz' instead of number 
+    elif fizzbuzz % 15 == 0:  
+        print("FizzBuzz")                                          
+    
+    # print numbers! 
+    print(fizzbuzz) 
