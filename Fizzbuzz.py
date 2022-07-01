004e9b83762b7b2751b8c2c663d3720e545082b6
Tash Hunda <Tash Hunda>
2022-07-01 18:00:49 -0500

    program that runs

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,21 +1,23 @@
 #loop for 100 times i.e. range 
-for fizzbuzz in range(1,101):  
+for fizzbuzz in range(100):  
   
-  # divisible by 3, print 'Fizz' instead of number 
-    if fizzbuzz % 3 == 0:      
+    # divisible by 3 & 5, print 'FizzBuzz'  
+    # instead of number 
+    if fizzbuzz % 15 == 0:  
+        print("FizzBuzz")                                          
+        continue
+  
+    # divisible by 3, print 'Fizz' 
+    # instead of number 
+    elif fizzbuzz % 3 == 0:      
         print("Fizz")                                          
         continue
-
-    # divisible by  5, print 'Buzz' instead of number 
+  
+    # divisible by  5, print 'Buzz' 
+    # instead of number 
     elif fizzbuzz % 5 == 0:          
         print("Buzz")                                      
         continue
   
-     # divisible by 3 & 5, print 'FizzBuzz' instead of number 
-    elif fizzbuzz % 15 == 0:  
-        print("FizzBuzz")                                          
-        continue
-  
-    
     # print numbers 
     print(fizzbuzz) 
